90d3ed88ccb240965e4f61b6195aa43ce84c1348
SydVic <fabrizio.bocca27@gmail.com>
2022-05-05 16:00:35 +0200

    aggiunto testo e img nel dom con vuejs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ Vue.config.devtools = true;
 
 const app = new Vue (
     {
-        el: "#root"
+        el: "#root",
+        data: {
+            domText: "già lo amo VueJS",
+            domImg: "https://miro.medium.com/max/800/1*Mfaj72PmdI9N5QanosaEPg.png"
+        }
     }
 );
